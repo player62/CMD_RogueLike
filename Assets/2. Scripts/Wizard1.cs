530ca27efcc8ab83a9e4c470eae3776ae306ada9
player62 <uri42012w@naver.com>
2022-11-21 16:11:28 +0900

    Wizard1

---
+++ Assets/2. Scripts/Wizard1.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wizard1 :Enemy
+{
+    private Rigidbody2D myRigidbody;
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
+    public Animator anim;
+    void Start()
+    {
+        currentState = EnemyState.idle;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+        CheckDistance();
+    }
+            private void SetAnimFloat(Vector2 setVector)
+    {
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+    private void changeAnim(Vector2 direction)
+    {
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if (direction.x > 0)
+            {
+                SetAnimFloat(Vector2.right);
+                transform.localScale = new Vector3(1, 1, 1); // 오른쪽 바라보기
+            }
+            else if (direction.x < 0)
+            {
+                SetAnimFloat(Vector2.left);
+                transform.localScale = new Vector3(-1, 1, 1); // 왼쪽 바라보기
+            }
+        }
+        else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if (direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+
+            }
+            else if (direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
+            }
+        }
+    }
+
+    private void ChangeState(EnemyState newState)
+    {
+        if (currentState != newState)
+        {
+            currentState = newState;
+        }
+    }
+    void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius
+            && Vector3.Distance(target.position, transform.position) > attackRadius)
+        {
+            if (currentState == EnemyState.idle || currentState == EnemyState.walk && currentState != EnemyState.stagger)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                changeAnim(temp - transform.position);
+                myRigidbody.MovePosition(temp);
+
+                ChangeState(EnemyState.walk);
+                anim.SetBool("Chase", true);
+            }
+            else if (Vector3.Distance(target.position, transform.position) > chaseRadius)
+            {
+                ChangeState(EnemyState.idle);
+                anim.SetBool("Chase", false);
+            }
+        }
+
+    }
+}
